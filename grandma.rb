0a6562d3bb9362cdc40740c32f2cbd574f4a9be1
Olivier Willems <ojwillems@gmail.com>
2018-08-02 16:43:15 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,11 +1,7 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-<<<<<<< HEAD
   elsif phrase == phrase.upcase
-=======
-  elseif phrase == phrase.upcase
->>>>>>> b5c8c7101d8d7dadfb21c8824c11e6d0cab6630f
     return "NO, NOT SINCE 1938!"
   else
     return "HUH?! SPEAK UP, SONNY!"
